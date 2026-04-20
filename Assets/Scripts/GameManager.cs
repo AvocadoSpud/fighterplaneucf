510296341e0ae8b3991893b713cd87cf549e0042
AvocadoSpud <atrickyrat2@gmail.com>
2026-04-19 21:46:50 -0400

    update with coins and shield and sounds and stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
-
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject gameOverMenu;
+    public GameObject audioPlayer;
+    public AudioClip powerUpSound;
+    public AudioClip powerDownSound;
     //public GameObject enemyOnePrefab;
     public GameObject[] enemyPrefabs;
     public GameObject cloudPrefab;
+    public GameObject powerUpPrefab;
+    public GameObject coinPrefab;
+    public TextMeshProUGUI powerUpText;
+    public TextMeshProUGUI scoreText;
+
 
     public float horizontalScreenSize;
     public float verticalScreenSize;
@@ -15,20 +25,101 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI livesText;
     public int score;
 
+    private bool gameOver;
+
+    public float powerUpDespawnTime = 4;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         horizontalScreenSize = 10f;
         verticalScreenSize = 6.5f;
-
+        gameOver = false;
         CreateSky();
         score = 0;
 
         InvokeRepeating("CreateEnemyOne", 1f, 2f);
         InvokeRepeating("CreateEnemyTwo", 5f, 4f);
 
+        StartCoroutine(SpawnPowerUp());
+        StartCoroutine(SpawnCoin());
+
+        powerUpText.text = "No Power-Up";
     }
+
+    private void Update()
+    {
+        if(gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    IEnumerator SpawnPowerUp()
+    {
+        float spawnTime = Random.Range(3, 5);
+        yield return new WaitForSeconds(spawnTime);
+        CreatePowerUp();
+        StartCoroutine(SpawnPowerUp());
+    }
+
+    IEnumerator SpawnCoin()
+    {
+        float spawnTime = Random.Range(1, 7);
+        yield return new WaitForSeconds(spawnTime);
+        CreateCoin();
+        StartCoroutine(SpawnCoin());
+    }
+
+
+    public void ManagePowerupText(int powerUpType)
+    {
+        switch (powerUpType)
+        {
+            case 1:
+                powerUpText.text = "Speed!";
+                break;
+            case 2:
+                powerUpText.text = "Double Weapons!";
+                break;
+            case 3:
+                powerUpText.text = "Triple Weapon!";
+                break;
+            case 4:
+                powerUpText.text = "Shield!";
+                break;
+            default:
+                powerUpText.text = "No Power-Up";
+                break;
+
+
+        }
+    }
+
+    public void PlaySound(int whichSound)
+    {
+        switch (whichSound)
+        {
+            case 1:
+                audioPlayer.GetComponent<AudioSource>().PlayOneShot(powerUpSound);
+                break;
+            case 2:
+                audioPlayer.GetComponent<AudioSource>().PlayOneShot(powerDownSound);
+                break;
+        }
+    }
+
+    void CreatePowerUp()
+    {
+        GameObject powerUp = Instantiate(powerUpPrefab, new Vector3(Random.Range(-horizontalScreenSize * .8f, horizontalScreenSize * .8f), Random.Range(-verticalScreenSize *.8f, 0), 0), Quaternion.identity);
+        Destroy(powerUp, powerUpDespawnTime);
+    }
+    void CreateCoin()
+    {
+        GameObject coin = Instantiate(coinPrefab, new Vector3(Random.Range(-horizontalScreenSize * .8f, horizontalScreenSize * .8f), Random.Range(-verticalScreenSize * .8f, 0), 0), Quaternion.identity);
+        Destroy(coin, powerUpDespawnTime);
+    }
+
 
     void CreateEnemyOne()
     {
@@ -49,10 +140,23 @@ public class GameManager : MonoBehaviour
     public void AddScore(int earnedScore)
     {
         score = score + earnedScore;
+        ChangeScoreText(score);
+    }
+    void ChangeScoreText(int score)
+    {
+        scoreText.text = "Score:" + score;
     }
 
     public void ChangeLivesText (int currentLives)
     {
         livesText.text = "lives: " + currentLives;
+    }
+
+    public void GameOver()
+    {
+        //set game over object menu to true
+        gameOverMenu.SetActive(true);
+        //game over to be true
+        gameOver = true;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,7 +10,12 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
     public int lives;
-    
+
+    public GameObject thruster;
+    public GameObject shield;
+
+    public int weaponType;
+    public bool shieldActive;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,15 +35,47 @@ public class PlayerController : MonoBehaviour
 
     public void LoseALife()
     {
-        lives--;
+        if (shieldActive)
+        {
+            shieldActive = false;
+        }
+        else
+        {
+            lives--;
+        }
         gameManager.ChangeLivesText(lives);
         if(lives == 0)
         {
             Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+            gameManager.GameOver();
+
             Destroy(gameObject);
         }
     }
-
+    IEnumerator ShieldPowerDown()
+    {
+        yield return new WaitForSeconds(5);
+        shield.SetActive(false);
+        shieldActive = false;
+        gameManager.PlaySound(2);
+        gameManager.ManagePowerupText(5);
+    }
+    IEnumerator SpeedPowerDown()
+    {
+        yield return new WaitForSeconds(5);
+        playerSpeed = 5f;
+        thruster.SetActive(false);
+        gameManager.PlaySound(2);
+        gameManager.ManagePowerupText(5);
+    }
+    IEnumerator WeaponPowerDown()
+    {
+        yield return new WaitForSeconds(5);
+        weaponType = 1;
+        gameManager.PlaySound(2);
+        gameManager.ManagePowerupText(5);
+    }
     void Movement()
     {
         horizontalInput = Input.GetAxis("Horizontal");
@@ -64,6 +102,52 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(bulletPrefab, transform.position + new Vector3(0,1,0), Quaternion.identity);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Coin"))
+        {
+            gameManager.AddScore(1);
+            Destroy(collision.gameObject);
+        }
+        if (collision.CompareTag("Powerup"))
+        {
+            Destroy(collision.gameObject);
+            int whichPowerup = Random.Range(1, 5);
+            gameManager.PlaySound(1);
+            switch (whichPowerup)
+            {
+                case 1:
+                    playerSpeed = 10;
+                    // start speed coroutine down
+                    // thruster active
+                    thruster.SetActive(true);
+                    StartCoroutine(SpeedPowerDown());
+                    break;
+                case 2:
+                    // set weapon type to 2
+                    weaponType = 2;
+                    // weapon power down coroutine
+                    StartCoroutine(WeaponPowerDown());
+                    break;
+                case 3:
+                    // set weapon type to 3
+                    weaponType = 3;
+                    // weapon power down
+                    StartCoroutine(WeaponPowerDown());
+                    break;
+
+                case 4:
+                    //set shield active
+                    //set the shield power down coroutine
+                    shield.SetActive(true);
+                    shieldActive = true;
+                    StartCoroutine(ShieldPowerDown());
+                    break;
+            }
+            gameManager.ManagePowerupText(whichPowerup);
         }
     }
 }
